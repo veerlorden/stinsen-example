514c665839f96edfbd2577aee21a963ec1f7d2a0
veerlorden <mailboxofvadim@gmail.com>
2022-10-19 21:47:27 +0300

    Initial Commit

---
+++ PopToRootStinsenSwiftUI/PopToRootStinsenSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopToRootStinsenSwiftUIApp.swift
+//  PopToRootStinsenSwiftUI
+//
+//  Created by Вадим Буркин on 19.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct PopToRootStinsenSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
